ca9f0372f578f5a6303f382a8dd14f69adec8430
Jason Zou <31863354+jzou9125@users.noreply.github.com>
2021-04-30 16:35:02 -0400

    userstory 8 to dev
    #8  to dev

--- Strangers/stranger1.py
+++ Strangers/stranger1.py
@@ -5,31 +5,28 @@ from text import *
 import random
 
 class Stranger:
-
     def __init__(self, game):
         self.game = game
         
         # instantiate select a person text 
-        self.person_text1 = Text('Traveler JACK:',650 ,130, 25, WHITE)
-        self.person_text2 = Text('Better take extra sets of clothes. The summer is kicking in.',650 ,230, 25, WHITE)
-        self.person_text3 = Text('Enjoy the journey while you can"',620 ,330, 25, WHITE)
+        self.person_text1 = Text('Traveler JACK:', 650, 130, 25, WHITE)
+        self.person_text2 = Text('Better take some extra clothes. The cold is starting to kick in.', 650, 230, 25, WHITE)
+        self.person_text3 = Text('Enjoy the journey while you can...', 620, 330, 25, WHITE)
 
-        # instantiate STRANGER3 button
-        self.back_button = Button(540, 500, 200, 70, LIGHT_GRAY, False, self.game)
-        self.back_button_text = Text('Back', 640, 540, 30, BLACK)
-        
+        # instantiate BACK button
+        self.back_button = Button(540, 520, 200, 70, LIGHT_GRAY, False, self.game)
+        self.back_button_text = Text('Back', 640, 560, 30, BLACK)
 
     def draw_screen(self):
-        #stranger's text
+        # stranger's text
         self.person_text1.draw(self.game.screen)
         self.person_text2.draw(self.game.screen)
         self.person_text3.draw(self.game.screen)
-       
-        # Back button
+
+        # BACK button
         self.back_button.draw(self.game.screen)
         self.back_button_text.draw(self.game.screen)
 
     def catch_actions(self, event, mx, my):
-
-        # catch Back button click
-        self.back_button.change_menu(event, mx, my, "talkToStranger")  
+        # catch BACK button click
+        self.back_button.change_menu(event, mx, my, "stranger")
--- menus/pitstop.py
+++ menus/pitstop.py
@@ -1,163 +1,71 @@
 import pygame
 from text import *
 from button import *
-from healthbar import *
 
-class Pitstop:
+class PitStop:
     def __init__(self, game):
         self.game = game
 
-        # instantiate campfire image
-        self.campfire = pygame.image.load('assets/campfire.png')
-        self.campfire = pygame.transform.scale(self.campfire, (475, 257))
+        # instantiate Pit stop header
+        self.pit_stop = Text('Pit Stop', self.game.width/2, 130, 60, WHITE)
 
-        x_offset = 100
-        y_offset = 50
-        # instantiate REST button
-        self.resume_button = Button(x_offset, y_offset+80, 200, 70, LIGHT_GRAY, False, self.game)
-        self.resume_button_text = Text('Rest', x_offset+100, y_offset+115, 30, BLACK)
-        # instantiate MEDKIT button
-        self.save_button = Button(x_offset, y_offset+180, 200, 70, LIGHT_GRAY, False, self.game)
-        self.save_button_text = Text('Medkit', x_offset+100, y_offset+217, 30, BLACK)
-        # instantiate STRANGER button
-        self.stranger_button = Button(x_offset, y_offset+280, 200, 70, LIGHT_GRAY, False, self.game)
-        self.stranger_button_text = Text('Strangers', x_offset+100, y_offset+320, 27, BLACK)
-        # instantiate JOBS button
-        self.jobs_button = Button(x_offset, y_offset+380, 200, 80, LIGHT_GRAY, False, self.game)
-        self.jobs_button_text = Text('Jobs', x_offset+100, y_offset+420, 30, BLACK)
-
-        x_offset2 = 0
-        y_offset2 = 640
-        # instantiate PARTY button
-        self.party_button = Button(x_offset2+70, y_offset2, 200, 80, LIGHT_GRAY, False, self.game)
-        self.party_button_text = Text('Party', x_offset2+170, y_offset2+43, 30, BLACK)
-        # instantiate SUPPLIES button
-        self.supplies_button = Button(x_offset2+300, y_offset2, 200, 80, LIGHT_GRAY, False, self.game)
-        self.supplies_button_text = Text('Supplies', x_offset2+400, y_offset2+43, 30, BLACK)
-        # instantiate BACK button
-        self.back_button = Button(x_offset2+950, y_offset2, 200, 80, LIGHT_GRAY, False, self.game)
-        self.back_button_text = Text('Back', x_offset2+1050, y_offset2+43, 30, BLACK)
-
-        x_offset3 = 260
-        y_offset3 = 85
-        # instantiate GET MONEY text
-        self.getmoney_text = Text('Get Money', x_offset3+100, y_offset3+130, 35, WHITE)
-        # instantiate BLACKJACK button
-        self.blackjack_button = Button(x_offset3, y_offset3+180, 200, 70, LIGHT_GRAY, False, self.game)
-        self.blackjack_button_text = Text('Blackjack', x_offset3+103, y_offset3+217, 23, BLACK)
-        # instantiate SLOT MAcHINE button
-        self.slotmachine_button = Button(x_offset3, y_offset3+280, 200, 70, LIGHT_GRAY, False, self.game)
-        self.slotmachine_button_text = Text('Slot Machine', x_offset3+100, y_offset3+320, 22, BLACK)
-
-        stat_x = 800
-        stat_y = 300
-        self.supply_count = Text('Supply Count', stat_x+90, stat_y-60, 35, WHITE)
-        self.fuel_text = Text('Fuel: {}'.format(self.game.fuel), stat_x+89, stat_y, 27, WHITE)
-        self.food_text = Text('Food: {}'.format(self.game.food), stat_x+100, stat_y+40, 27, WHITE)
-        self.money_text = Text('Money: {}'.format(self.game.money), stat_x+104, stat_y+80, 27, WHITE)
-
-        # usernames
-        user_x = 800
-        user_y = 30
-        self.car_text = Text('Kia SUV', user_x, user_y, 27, WHITE)
-        self.u1_text = Text('User 1', user_x, user_y+40, 27, WHITE)
-        self.u2_text = Text('User 2', user_x, user_y+80, 27, WHITE)
-        self.u3_text = Text('User 3', user_x, user_y+120, 27, WHITE)
-        self.u4_text = Text('User 4', user_x, user_y+160, 27, WHITE)
-        # health bars
-        self.u1_health = Healthbar(self.game, user_x+105, user_y+23, "u1")
-        self.u2_health = Healthbar(self.game, user_x+105, user_y+63, "u2")
-        self.u3_health = Healthbar(self.game, user_x+105, user_y+103, "u3")
-        self.u4_health = Healthbar(self.game, user_x+105, user_y+143, "u4")
+        # instantiate Map button
+        self.map_button = Button(360, 210, 200, 70, LIGHT_GRAY, False, self.game)
+        self.map_button_text = Text('Map',460 , 250, 30, BLACK)
+        # instantiate Heal button
+        self.heal_button = Button(700, 210, 200, 70, LIGHT_GRAY, False, self.game)
+        self.heal_button_text = Text('Heal', 800, 250, 30, BLACK)
+        # instantiate Talk to stranger button
+        self.talk_to_stranger_button = Button(360, 310, 200, 70, LIGHT_GRAY, False, self.game)
+        self.talk_to_stranger1_button_text = Text('Talk to', 460,340, 20, BLACK)
+        self.talk_to_stranger2_button_text = Text('Stranger', 460,360, 20, BLACK)
+        # instantiate Shop button
+        self.shop_button = Button(700, 310, 200, 70, LIGHT_GRAY, False, self.game)
+        self.shop_button_text = Text('Shop', 800, 350, 30, BLACK)
+        # instantiate Scavenge button
+        self.scavenge_button = Button(360, 410, 200, 70, LIGHT_GRAY, False, self.game)
+        self.scavenge_button_text = Text('Scavenge', 460, 450, 30, BLACK)
+        # instantiate Jobs button
+        self.job_button = Button(700, 410, 200, 70, LIGHT_GRAY, False, self.game)
+        self.job_button_text = Text('Jobs', 800, 450, 30, BLACK)
+        # instantiate Resume button
+        self.resume_button = Button(530, 610, 200, 70, LIGHT_GRAY, False, self.game)
+        self.resume_button_text = Text('Resume', 630, 650, 30, BLACK)
 
     def draw_screen(self):
-        # draw PARTY button
-        self.party_button.draw(self.game.screen)
-        self.party_button_text.draw(self.game.screen)
-        # draw SUPPLIES button
-        self.supplies_button.draw(self.game.screen)
-        self.supplies_button_text.draw(self.game.screen)
-        # draw BACK button
-        self.back_button.draw(self.game.screen)
-        self.back_button_text.draw(self.game.screen)
+        # draw Pit Stop header
+        self.pit_stop.draw(self.game.screen)
 
-        if self.game.pitstop_menu == "party":
-            # shade buttons appropriately
-            self.party_button.color = DARK_GRAY
-            self.supplies_button.color = LIGHT_GRAY
-
-            # draw campfire
-            self.game.screen.blit(self.campfire, (700,275))
-
-            # draw REST button
-            self.resume_button.draw(self.game.screen)
-            self.resume_button_text.draw(self.game.screen)
-            # draw MEDKIT button
-            self.save_button.draw(self.game.screen)
-            self.save_button_text.draw(self.game.screen)
-            # draw STRANGER button
-            self.stranger_button.draw(self.game.screen)
-            self.stranger_button_text.draw(self.game.screen)
-            # draw JOBS button
-            self.jobs_button.draw(self.game.screen)
-            self.jobs_button_text.draw(self.game.screen)
-
-            # draw usernames
-            self.u1_text.draw(self.game.screen)
-            self.u2_text.draw(self.game.screen)
-            self.u3_text.draw(self.game.screen)
-            self.u4_text.draw(self.game.screen)
-
-            # draw health bars
-            self.u1_health.draw(self.game.screen)
-            self.u2_health.draw(self.game.screen)
-            self.u3_health.draw(self.game.screen)
-            self.u4_health.draw(self.game.screen)
-
-        elif self.game.pitstop_menu == "supplies":
-            # shade buttons appropriately
-            self.party_button.color = LIGHT_GRAY
-            self.supplies_button.color = DARK_GRAY
-
-            # GET MONEY text
-            self.getmoney_text.draw(self.game.screen)
-
-            # draw BLACKJACK button
-            self.blackjack_button.draw(self.game.screen)
-            self.blackjack_button_text.draw(self.game.screen)
-            # draw SLOT MACHINE button
-            self.slotmachine_button.draw(self.game.screen)
-            self.slotmachine_button_text.draw(self.game.screen)
-
-            # draw "Supply Count" Text
-            self.supply_count.draw(self.game.screen)
-            # draw supplies count
-            self.fuel_text.text = 'Fuel: {}'.format(self.game.fuel)    # fuel
-            self.fuel_text.draw(self.game.screen)
-            self.food_text.text = 'Food: {}'.format(self.game.food)    # food
-            self.food_text.draw(self.game.screen)
-            self.money_text.text = 'Money: {}'.format(self.game.money)        # money
-            self.money_text.draw(self.game.screen)
+        # draw buttons
+        # MAP button
+        self.map_button.draw(self.game.screen)
+        self.map_button_text.draw(self.game.screen)
+        # HEAL button
+        self.heal_button.draw(self.game.screen)
+        self.heal_button_text.draw(self.game.screen)
+        # Talk to Stranger button
+        self.talk_to_stranger_button.draw(self.game.screen)
+        self.talk_to_stranger1_button_text.draw(self.game.screen)
+        self.talk_to_stranger2_button_text.draw(self.game.screen)
+        # SHOP button
+        self.shop_button.draw(self.game.screen)
+        self.shop_button_text.draw(self.game.screen)
+        # SCAVENGE button
+        self.scavenge_button.draw(self.game.screen)
+        self.scavenge_button_text.draw(self.game.screen)
+        # JOB button
+        self.job_button.draw(self.game.screen)
+        self.job_button_text.draw(self.game.screen)
+        # resume button
+        self.resume_button.draw(self.game.screen)
+        self.resume_button_text.draw(self.game.screen)
 
     def catch_actions(self, event, mx, my):
-        # catch REST button click
-        # catch MEDKIT button click
-        # catch STRANGER button click
-        self.stranger_button.change_menu(event, mx, my, "stranger")
-        # catch JOBS button click
-        self.jobs_button.change_menu(event, mx, my, "jobs")
-
-        # catch SLOT MACHINE play
-        self.slotmachine_button.change_menu(event, mx, my, "slotmachine")
-        # catch BLACKJACK play
-        self.blackjack_button.change_menu(event, mx, my, "blackjack")
-
-        # catch UI changes
-        self.party_button.change_ui(event, mx, my, "pitstop", "party")
-        self.supplies_button.change_ui(event, mx, my, "pitstop", "supplies")
-
-        # catch BACK button click
-        self.back_button.change_menu(event, mx, my, "play")
+        
+        # catch SAVE button click
+        self.talk_to_stranger_button.change_menu(event, mx, my, "talkToStranger")
+        # catch RESUME button click
+        self.resume_button.change_menu(event, mx, my, "play")
 
         # press esc key to UNPAUSE
         if event.type == pygame.KEYDOWN and event.key == K_ESCAPE:
